b3f0097112f1d3680aaa19a3679f22c7d4dd4b83
Garreth Cain <garrethccain@gmail.com>
2026-03-05 06:39:36 +1000

    fix: renamed the appconfig classes

--- easyjwt_client/apps.py
+++ easyjwt_client/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ClientAppConfig(AppConfig):
+class EasyJWTClientConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "easyjwt_client"
